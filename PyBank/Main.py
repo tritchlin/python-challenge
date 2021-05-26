ce1baa50b55c770d05032fd697bd03a82ff3980c
tritchlin <annylin@gmail.com>
2021-05-25 19:27:45 -0700

    Updated Main.py file

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -1,16 +1,5 @@
 # In this challenge, you are tasked with creating a Python script for analyzing the financial records of your company. You will give a set of financial data called budget_data.csv. The dataset is composed of two columns: Date and Profit/Losses. (Thankfully, your company has rather lax standards for accounting so the records are simple.)
-
 # Your task is to create a Python script that analyzes the records to calculate each of the following:
-
-# The total number of months included in the dataset
-
-# The net total amount of "Profit/Losses" over the entire period
-
-# Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
-
-# The greatest increase in profits (date and amount) over the entire period
-
-# The greatest decrease in profits (date and amount) over the entire period
 
 # As an example, your analysis should look similar to the one below:
 # Financial Analysis
@@ -25,11 +14,64 @@
 
 import os
 import csv
+import locale
 
-csvpath = os.path.join('Resources','budget_data.csv')
+from numpy.lib.function_base import select
 
-with open(csvpath,'r') as csvfile:
-    csvreader = csv.reader(csvfile,delimiter=",")
+locale.setlocale(locale.LC_ALL,'en_US')
 
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+# Locate and open CSV file
+csvpath = os.path.join("Resources","budget_data.csv")
+csvfile = open(csvpath,'r')
+budget = csv.reader(csvfile)
+
+#remove heade3rs
+csv_header = next(budget)
+
+#create lists
+month = []
+data = []
+months = []
+datas = []
+
+#append column info into lists
+for row in budget:
+    month = str(row[0])
+    data = int(row[1])
+    
+    months.append(month)
+    datas.append(data)
+    
+ # The total number of months included in the dataset
+count = len(months)
+
+print(count)
+
+# The net total amount of "Profit/Losses" over the entire period
+total = sum(datas)
+
+print(locale.currency(total, grouping = True))
+
+# Identify min and max of "Profit/Losses", calculate average and difference
+
+lowest_amt = min(datas)
+highest_amt = max(datas)
+
+# print(lowest_amt)
+# print (highest_amt)
+
+difference = highest_amt - lowest_amt
+average = total/count
+
+print(locale.currency(difference, grouping = True))
+print(locale.currency(average, grouping = True))
+
+# The greatest increase in profits (date and amount) over the entire period
+print(locale.currency(highest_amt, grouping = True))
+if highest_amt == row[0]:
+        highest_date(row)
+
+print(highest_date)
+
+# The greatest decrease in profits (date and amount) over the entire period
+print(locale.currency(lowest_amt, grouping = True))
